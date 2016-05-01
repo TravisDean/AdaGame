444a36cb3842a1f6f22016b4d2462bc870fe8703
Travis Dean <travis.j.dean@gmail.com>
2016-05-01 00:03:18 -0400

    passes lexer

--- games.adb
+++ games.adb
@@ -9,7 +9,11 @@ with ada.integer_text_io; use ada.integer_text_io;
 with Ada.Strings.Unbounded; 
 with Stacks;
 with Queues;
-procedure playGame is
+
+procedure games is
+    --use queue for the print output (winner on top, followed by most recently eliminated)
+    --Use stack for the 3 stacks that have no losses, 1 loss, 2 losses (eliminated)
+
     package SU renames Ada.Strings.Unbounded;
     type Person is
         record
@@ -42,29 +46,6 @@ procedure playGame is
         return Person(pName, pSkill, pAge, index => currIndex);
     end getPerson;
 
-    -- first by skill, then age, then index
-    function "<" (Left, Right : Person) return Boolean is
-    begin
-        if left.skill /= right.skill then
-            return left.skill < right.skill;
-        elsif left.age /= right.age then
-            return left.age < right.age;
-        else
-            return left.index < right.index
-        end if;
-    end "<";
-
-    procedure playMatch(p1: in out Person; p2: in out Person) is
-    begin
-        if p1 < p2 then
-            p1.losses := p1.losses + 1;
-            p2.wins := p2.wins + 1;
-        else
-            p2.losses := p2.losses + 1;
-            p1.wins := p1.wins + 1;
-        end if;
-    end playMatch;
-
     function getPlayers return Integer is
         index : Integer := 0;
     begin
@@ -80,7 +61,7 @@ procedure playGame is
         numPlayers : Integer;
         best : Person;
     begin
-        numPlayers := getPlayers();
+        numPlayers := getPlayers;
         if numPlayers = 0 then
             exit;
         elsif numPlayers = 1 then
@@ -90,50 +71,50 @@ procedure playGame is
 
         while not is_Empty(zeroLoss) loop
             -- Normal case: get two, check match, enqueue
-           p1 := front(zeroLoss); dequeue(zeroLoss);
-           -- Handle odd and even amounts
-           if not is_Empty(zeroLoss) then
-               p2 := front(zeroLoss); dequeue(zeroLoss);
-               playMatch(p1, p2);   -- Modifying func
-               if p1.losses = 0 then
-                   enqueue(p1, zeroLoss);
-                   enqueue(p2, oneLoss);
-               elsif
-                   enqueue(p2, zeroLoss);
-                   enqueue(p1, oneLoss);
-               end if;
-            -- The odd num left case
-            else then
+            p1 := front(zeroLoss); dequeue(zeroLoss);
+            -- Handle odd and even amounts
+            if not is_Empty(zeroLoss) then
+                p2 := front(zeroLoss); dequeue(zeroLoss);
+                playMatch(p1, p2);   -- Modifying func
+                if p1.losses = 0 then
+                    enqueue(p1, zeroLoss);
+                    enqueue(p2, oneLoss);
+                else
+                    enqueue(p2, zeroLoss);
+                    enqueue(p1, oneLoss);
+                end if;
+                -- The odd num left case
+            else 
                 best := p1; 
-           end if;
+            end if;
         end loop;
 
         while not is_Empty(oneLoss) loop
             -- Normal case: get two, check match, enqueue
-           p1 := front(oneLoss); dequeue(oneLoss);
-           -- Handle odd and even amounts
-           if not is_Empty(oneLoss) then
-               p2 := front(oneLoss); dequeue(oneLoss);
-               playMatch(p1, p2);   -- Modifying func
-               if p1.losses = 1 then
-                   enqueue(p1, oneLoss);
-                   push(p2, done);
-               elsif
-                   enqueue(p2, oneLoss);
-                   push(p1, done);
-               end if;
-            -- The odd num left case
-            else then   -- so p1 must face our best for the title
-               playMatch(best, p1);
-               if best.losses = 0 then
-                   push(p1, done);
-                   push(best, done);
-               elsif            -- All this double push/enq stuff could be 
-                                -- simplified with pointers
-                   push(best, done);
-                   push(p1, done);
-               end if;
-           end if;
+            p1 := front(oneLoss); dequeue(oneLoss);
+            -- Handle odd and even amounts
+            if not is_Empty(oneLoss) then
+                p2 := front(oneLoss); dequeue(oneLoss);
+                playMatch(p1, p2);   -- Modifying func
+                if p1.losses = 1 then
+                    enqueue(p1, oneLoss);
+                    push(p2, done);
+                else
+                    enqueue(p2, oneLoss);
+                    push(p1, done);
+                end if;
+                -- The odd num left case
+            else -- so p1 must face our best for the title
+                playMatch(best, p1);
+                if best.losses = 0 then
+                    push(p1, done);
+                    push(best, done);
+                else            -- All this double push/enq stuff could be 
+                    -- simplified with pointers
+                    push(best, done);
+                    push(p1, done);
+                end if;
+            end if;
         end loop;
 
         -- So now we have to print off the results
@@ -141,24 +122,10 @@ procedure playGame is
             print(top(done));
             pop(done);
         end loop;
-begin
-
-
- 
-procedure games is
---use queue for the print output (winner on top, followed by most recently eliminated)
---Use stack for the 3 stacks that have no losses, 1 loss, 2 losses (eliminated)
-    --
-
-    
+    end gameloop;
 
 Begin
-
-
-
-
-
-
+    gameloop;
 
 
 end games;
